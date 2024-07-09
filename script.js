89849f9b27f0571e32b1090d2bd536c684d613df
Richard Lamb <lamby42@live.co.uk>
2024-07-09 15:11:48 +0100

    added .isarray borken code

--- script.js
+++ script.js
@@ -8,4 +8,14 @@ let shoppingList = [
     3,
 ];
 
-console.log(Array.isArray (shoppingList))
+console.log(Array.isArray (shoppingList));
+
+//how do we target an array within another array and check it is an array
+
+const Array = ["one"]
+
+const someArray = ["one", 6, ["a", "b", "c"]];
+
+//Array.isArray(someArray[2][1]);
+
+//console.log(Array.isArray (shoppingList));
